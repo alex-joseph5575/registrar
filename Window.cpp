6b0787dd8683892a861def18e58ed05483aa563c
Alex Joseph <alex.joseph5575@gmail.com>
2019-11-05 21:06:53 +0000

    Window class

---
+++ Window.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+Window::Window()
+{
+  isOpen = true;
+  idleTime = 0;
+  responseTime = 0;
+}
+
+Window::~Window()
+{
+
+}
---
+++ Window.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+class Window
+{
+  public:
+    bool isOpen;
+    int idleTime;
+    int responseTime;
+
+    Window();
+    ~Window();
+};
